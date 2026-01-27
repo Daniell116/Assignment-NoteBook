d25eff66a8ad7a0bce04ce14465a4de05ab7ca05
daniell escareno <daniellescareno7@gmail.com>
2026-01-27 06:38:13 -0800

    Initial Commit

---
+++ Assignment NoteBook/Assignment_NoteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NoteBookApp.swift
+//  Assignment NoteBook
+//
+//  Created by daniell Escareno on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NoteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
