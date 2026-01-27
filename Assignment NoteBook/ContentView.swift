1b8dd9882148ab9a17a8615f0f79add81ac516bc
daniell escareno <daniellescareno7@gmail.com>
2026-01-27 08:41:39 -0800

    Created initial Homework list

--- Assignment NoteBook/ContentView.swift
+++ Assignment NoteBook/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dueDate = Date()
+    @State private var description = ""
+    @State private var  toDoHomework =
+    [ToDoHomework(priority: "Math", description: "Linear", dueDate: Date()),
+     ToDoHomework(priority: "History", description: "Study test", dueDate: Date()),
+     ToDoHomework(priority: "English", description: "Do reading", dueDate: Date())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(toDoHomework) { homework in
+                    Text(homework.description)
+                }
+                .onMove(perform: { indices, newOffset in
+                    toDoHomework.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    toDoHomework.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Homework", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+struct ToDoHomework: Identifiable {
+    var id = UUID()
+    var priority = String()
+    var description = String()
+    var dueDate = Date()
+    
 }
